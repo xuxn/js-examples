cd0bf95f0845f37c708027c63403208b653e5cdd
xxn <xuxn@Ctrip.com>
2017-09-11 15:31:08 +0800

    js-eample notification

--- 2017-9-8/js/index.js
+++ 2017-9-8/js/index.js
@@ -19,7 +19,7 @@ window.onload=function(){
 
 	//控制音频
 	var audio=document.getElementById("audio");
-	audio.play();
+	audio.pause();
 	audio.loop=true;
 
 	function changeAudio(event){
@@ -64,5 +64,31 @@ window.onload=function(){
 	window.addEventListener("unload",function(){
 		localStorage.removeItem('name'); 
 	},false);
+
+
+	//通知请求 
+	var button = document.getElementById('button'), text = document.getElementById('text');
+	var options={
+		body:"祝福老薛，一往情深彼此都给了同一个人，最幸福莫过于兜兜转转，你还在我身边要好好的，一直走下去。不想再寻觅 那就再爱一次吧 !",
+		icon:'http://image.zhangxinxu.com/image/study/s/s128/mm1.jpg',
+		sound:'http://www.w3school.com.cn/i/horse.ogg',
+		silent:true 
+	}
+	var title="下一首歌曲";
+	button.addEventListener("click",function(){
+		Notification.requestPermission(function(){
+			if(Notification.permission=='granted'){
+				var n = new Notification(title, options);  
+				n.onclick=function(){ 
+					text.innerHTML="歌曲已播放";
+					n.close();
+				}
+			}else{ 
+				Notification.permission=='default';
+				console.log("您已拒绝发送通知");
+			}
+		})
+	},false);
+
 
 }
